bcafd5bf1ed1648460321cf1226387f744692f22
krizi <krazy2krizi@gmail.com>
2016-04-15 11:36:47 +0200

    Just kurac.

---
+++ src/ch/sandbox/ego/Run.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ch.sandbox.ego;
+
+/**
+ * @author p105324
+ *
+ */
+public class Run {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("kurac");
+
+	}
+
+}
